d043d547315a19028b09d754975fc5e7943699f4
DineshBabu10 <69631307+DineshBabu10@users.noreply.github.com>
2025-06-20 02:34:48 +0000

    changes

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+job_list = [{'id':1, 'title':'Data Analyst', 'location':'Bengaluru', 'salary':'10,00,000'},
+             {'id':2, 'title':'Data Scientist', 'location':'Delhi', 'salary':'15,00,000'}]
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('home.html', company_name='Jovian', jobs= job_list)
 
 
 if __name__ == '__main__':
